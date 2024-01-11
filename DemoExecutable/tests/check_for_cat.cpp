89865b51c03c59495979e836cd4fc6a2e6f6f12a
ClawmanCat <clawmancat@gmail.com>
2024-01-11 22:55:35 +0100

    Clean up debug code

--- DemoExecutable/tests/check_for_cat.cpp
+++ DemoExecutable/tests/check_for_cat.cpp
@@ -1,15 +1,11 @@
 #include <filesystem>
 #include <cstdlib>
-#include <iostream>
 
 
 namespace fs = std::filesystem;
 
 
 int main() {
-    const auto cat_path = fs::absolute(fs::path { DEMOEXECUTABLE_ASSETS_DIR "/cat_pictures/cat.png" });
-    std::cout << "Checking for the existence of " << cat_path.string() << std::endl;
-    
-    if (fs::exists(cat_path)) return EXIT_SUCCESS;
+    if (fs::exists(fs::path { DEMOEXECUTABLE_ASSETS_DIR "/cat_pictures/cat.png" })) return EXIT_SUCCESS;
     else return EXIT_FAILURE;
 }
